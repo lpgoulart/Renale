dce47db3eb5f5d2a3888751ca48fcb0dce545290
lpgoulart <leonardo@roffor.com.br>
2018-11-26 16:28:08 -0300

    add dropdown function

--- js/index.js
+++ js/index.js
@@ -32,3 +32,66 @@ $(document).ready(function(){
     });
   });
 });
+
+function blogDropdown() {
+    document.getElementById("empresa").classList.remove("show");
+    document.getElementById("dialise").classList.remove("show");
+    document.getElementById("blog").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+function empresaDropdown() {
+    document.getElementById("blog").classList.remove("show");
+    document.getElementById("dialise").classList.remove("show");
+    document.getElementById("empresa").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+function dialiseDropdown() {
+    document.getElementById("blog").classList.remove("show");
+    document.getElementById("empresa").classList.remove("show");
+    document.getElementById("dialise").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
